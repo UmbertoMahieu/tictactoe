5967014b072863cb0bd0643f909d6c5e37451e67
UmbertoMahieu <umbertomahieu@gmail.com>
2025-01-17 00:27:16 +0900

    Add end game mechanic and recursive input if case has already been played

--- main.py
+++ main.py
@@ -15,30 +15,64 @@ def print_board(boardgame):
             print("--------------------------")
 
 
+def ask_pos():
+    pos = input("Send row number and case number to add your sign. eg: 1,2 for first row, second case : ")
+    position = tuple(map(int, pos.split(',')))
+    if boardgame[position[0]-1][position[1]-1] != 0:
+        return ask_pos()
+    else:
+        return position
+
 def turn(player):
     global boardgame
-    print(f"Player {player}")
-    pos = input("Send row number and case number to add your sign as (1,2) for first row, second case : ")
-    position = tuple(map(int, pos.split(',')))
+    print(f"--- Player {player} turn ---")
+    position = ask_pos()
     boardgame_pos = boardgame[position[0]-1][position[1]-1]
     if boardgame_pos == 0 :
         boardgame[position[0]-1][position[1]-1] = player
-    print(boardgame)
     print_board(boardgame)
+
+def check_win():
+    global boardgame
+    if boardgame[0][0] != 0 and boardgame[0][0] == boardgame[0][1] and boardgame[0][1] == boardgame[0][2]:
+        return check_winner(boardgame[0][0])
+    elif boardgame[1][0] != 0 and boardgame[1][0] == boardgame[1][1] and boardgame[1][1] == boardgame[1][2]:
+        return check_winner(boardgame[0][0])
+    elif boardgame[2][0] != 0 and boardgame[2][0] == boardgame[2][1] and boardgame[2][1] == boardgame[2][2]:
+        return check_winner(boardgame[0][0])
+    elif boardgame[0][0] != 0 and boardgame[0][0] == boardgame[1][0] and boardgame[1][0] == boardgame[2][0]:
+        return check_winner(boardgame[0][0])
+    elif boardgame[0][1] != 0 and boardgame[0][1] == boardgame[1][1] and boardgame[1][1] == boardgame[2][1]:
+        return check_winner(boardgame[0][1])
+    elif boardgame[0][2] != 0 and boardgame[0][2] == boardgame[1][2] and boardgame[1][2] == boardgame[2][2]:
+        return check_winner(boardgame[0][2])
+    elif boardgame[0][0] != 0 and boardgame[0][0] == boardgame[1][1] and boardgame[1][1] == boardgame[2][2]:
+        return check_winner(boardgame[0][0])
+    elif boardgame[0][2] != 0 and boardgame[0][2] == boardgame[1][1] and boardgame[1][1] == boardgame[2][0]:
+        return check_winner(boardgame[0][2])
+    else:
+        return False
+
+def check_winner(result):
+    if result == 1:
+        print("player 1 win !")
+    if result == 2:
+        print("player 2 win !")
+    return True
+
 
 boardgame = [[0,0,0],[0,0,0],[0,0,0]]
 player1 = 1
 player2 = 2
 
-print(boardgame)
 print_board(boardgame)
 
 while True:
     turn(player1)
+    if check_win():
+        break
     turn(player2)
-    for row in boardgame:
-        if sum(row) == 3:
-            print("player 1 win !")
-        elif sum(row) == 6:
-            print("player 2 win !")
+    if check_win():
+        break
+
 
